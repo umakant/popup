56c858225c7f8e9660c247a89a4135d90cdeedd3
Umakant Sonwani <umakantsonwani@gmail.com>
2017-07-21 00:21:17 +0530

    demo content

---
+++ pop.php
@@ -0,0 +1,11 @@
+<link rel="stylesheet" href="colorbox.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="jquery.colorbox.js"></script>
+    <script>
+        $(document).ready(function(){
+            $(".iframe").colorbox({iframe:true, width:"80%", height:"80%"});
+        });
+    </script>
+<body>
+    <p><a class='iframe' href="http://threadless.com">Outside Webpage (Iframe)</a></p>
+</body>
